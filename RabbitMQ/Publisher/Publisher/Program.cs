c4edfc8f8e18d69d2fba66d075836aaeceb6738d
barcelosleticia <leticia.fbarcelos@outlook.com>
2018-04-12 11:37:55 -0300

    Implementação
    Envio da implementação em dotnet.

---
+++ RabbitMQ/Publisher/Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "RabbitMQ",
+                       durable: false,
+                       exclusive: false,
+                       autoDelete: false,
+                       arguments: null);
+                    string message = "Publisher enviou ao Consumer!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                            routingKey: "RabbitMQ",
+                            basicProperties: null,
+                            body: body);
+                    Console.WriteLine("Teste rabbitMQ realizado!");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
